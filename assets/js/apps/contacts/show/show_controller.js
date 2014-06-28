runrobrun/marionette-gentle-introduction

add loading view to sub-module

assets/js/apps/contacts/show/show_controller.js
@@ -1,6 +1,9 @@
 ContactManager.module('ContactsApp.Show', function(Show, ContactManager, Backbone, Marionette, $, _){
   Show.Controller = {
     showContact: function(id){
+      var loading_view = new ContactManager.Common.Views.Loading();
+      ContactManager.mainRegion.show(loading_view);
+
       var fetching_contact = ContactManager.request("contact:entity", id);
       $.when(fetching_contact).done(function(contact){
         var contact_view;

simple-git-user/js_basic_marionette

assets/js/apps/contacts/show/show_controller.js
@@ -1,6 +1,9 @@
 ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbone, Marionette, $, _){
   Show.Controller = {
     showContact: function(id){
+      var loadingView = new ContactManager.Common.Views.Loading();
+      ContactManager.mainRegion.show(loadingView);
+
       var fetchingContact = ContactManager.request("contact:entity", id);
       $.when(fetchingContact).done(function(contact){
         var contactView;