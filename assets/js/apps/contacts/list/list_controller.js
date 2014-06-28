runrobrun/marionette-gentle-introduction

loading view for contacts list

assets/js/apps/contacts/list/list_controller.js
@@ -1,6 +1,9 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
   List.Controller = {
     listContacts: function(){
+      var loading_view = new ContactManager.Common.Views.Loading();
+      ContactManager.mainRegion.show(loading_view);
+
       var fetching_contacts = ContactManager.request("contact:entities");
       
       $.when(fetching_contacts).done(function(contacts){

simple-git-user/js_basic_marionette

assets/js/apps/contacts/list/list_controller.js
@@ -1,6 +1,9 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Controller = {
     listContacts: function(){
+      var loadingView = new ContactManager.Common.Views.Loading();
+      ContactManager.mainRegion.show(loadingView);
+
       var fetchingContacts = ContactManager.request("contact:entities");
 
       $.when(fetchingContacts).done(function(contacts){