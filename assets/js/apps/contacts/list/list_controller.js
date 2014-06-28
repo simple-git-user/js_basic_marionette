davidsulc/marionette-gentle-introduction

removing a contact from the collection

assets/js/apps/contacts/list/list_controller.js
@@ -7,6 +7,10 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
         collection: contacts
       });
 
+      contactsListView.on("itemview:contact:delete", function(childView, model){
+        contacts.remove(model);
+      });
+
       ContactManager.mainRegion.show(contactsListView);
     }
   }

simple-git-user/js_basic_marionette

assets/js/apps/contacts/list/list_controller.js
@@ -7,6 +7,10 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
         collection: contacts
       });
 
+      contactsListView.on("childview:contact:delete", function(childView, model){
+        contacts.remove(model);
+      });
+
       ContactManager.mainRegion.show(contactsListView);
     }
   }