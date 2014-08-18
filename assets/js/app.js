d5c9455b3a75f9e190dfcd5565abbee3f5a03db4
Vishwanath <vishwanaths@aditi.com>
2014-08-18 15:55:26 +0530

    adds a new module

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,3 +23,31 @@ ContactManager.on("start", function(){
     }
   }
 });
+
+ContactManager.module('articles',function(Entities, ContactManager, Backbone, Marionette, $, _){
+
+     var Article_v = Marionette.ItemView.extend({
+        template:"#article-item"
+    });
+    var Articles_v = Marionette.CollectionView.extend({
+        childView:Article_v
+    });
+    var Article = Backbone.Model.extend({});
+    var Articles = Backbone.Collection.extend({
+        model:Article
+    });
+    var a1 = new  Article({title:'t1'});
+    var a2 = new  Article({title:'t2'});
+    var a3 = new  Article({title:'t3'});
+
+    var arts = new Articles([a1, a2, a3]);
+
+    Entities.articles_v = new Articles_v({collection:arts});
+    Entities.articles_v.collection.add({titles:'123'});
+
+    ContactManager.addRegions({newApp: "#newApp"});
+    ContactManager.newApp.show(articles_v);
+    ContactManager.addInitializer(function(){});
+});
+
+
